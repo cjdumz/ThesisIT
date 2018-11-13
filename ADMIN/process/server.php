0a1d638ff8f7619b0bf8dcfe26fb2aa1eae0e0cc
Leo Evrian E. Diano <2132287@slu.edu.ph>
2018-11-13 16:26:38 +0800

    added sms

--- ADMIN/process/server.php
+++ ADMIN/process/server.php
@@ -19,24 +19,6 @@ if(isset($_POST['command1'])){
 
 }
 
-// //Accepting and Denying of Appointments
-// if(isset($_POST['commands1'])){
-//   $action = $connection->real_escape_string($_POST["command1"]);
-//   $id = $connection->real_escape_string($_POST["id1"]);
-
-  // if($action=='accept'){
-  //   $actions_command = $connection->prepare("UPDATE `appointments` SET `status` = 'Accepted', `modified` = NOW() WHERE `appointments`.`id` = $id ;");
-  // }else{
-  //   $actions_command = $connection->prepare("UPDATE `appointments` SET `status` = 'Declined', `modified` = NOW() WHERE `appointments`.`id` = $id ;");
-  // }
-  // if($actions_command ->execute()){
-  //   $MSG = "succesully approved appointment";
-  //   header("Refresh: 0; url=../appointments.php");
-  // }else{
-  //   $MSG = "there was an error while updating the data..";
-  // }
-// }
-
 if(isset($_POST['resubmit'])){
   $update = $connection->real_escape_string($_POST["update"]);
   $id = $connection->real_escape_string($_POST["id"]);
@@ -47,5 +29,34 @@ if(isset($_POST['resubmit'])){
     $MSG = "there was an error while updating the data..";
   }
 }
+
+if(isset($_POST["submit-user"])){
+  $id = $connection->real_escape_string($_POST["id"]);
+  $first = $connection->real_escape_string($_POST["first"]);
+  $middle = $connection->real_escape_string($_POST["middle"]);
+  $last = $connection->real_escape_string($_POST["last"]);
+  $suffix = $connection->real_escape_string($_POST["suffix"]);
+  $address = $connection->real_escape_string($_POST["address"]);
+  $email = $connection->real_escape_string($_POST["email"]);
+  $mobile = $connection->real_escape_string($_POST["mobile"]);
+  $telephone = $connection->real_escape_string($_POST["telephone"]);
+
+  echo $id,", ", $first,", ", $middle,", ", $last, ", ",$suffix,", ", $address,", ", $email,", ", $mobile,", ", $telephone;
+
+  $update = $connection->prepare("UPDATE `personalinfo` SET `firstName`= ?,
+  `lastName`= ?,`middleName`= ?,`suffix`= ?,`address`= ?,`mobileNumber`= ?,`telephoneNumber`= ?,
+  `email`= ?, `modified`= NOW() WHERE `personalId` = ?");
+
+  $update->bind_param("ssssssssi", $first, $last, $middle, $suffix, $address, $mobile, $telephone, $email, $id);
+  if($update->execute()){ 
+    header("Location: ../user.php?id=$id");
+    echo "Gumana";
+  }else{
+    header("Location: ../error.php");
+    exit();
+}
+}
 ?>
 
+<input type="text" name="phone" value="<?php $phone ?>">
+<textarea name="message" id="" cols="30" rows="10"></textarea>
--- ADMIN/viewVehicle.php
+++ ADMIN/viewVehicle.php
@@ -330,7 +330,7 @@ if(isset($_GET['plate'])){
                                            </div>
                                         </td>
                                         <td>'.$row['status'].'</td>
-                                        <td class="text-center"><a href="records.php?'.$row['id'].'"><button class="btn btn-primary">View</button></a></td>
+                                        <td class="text-center"><a href="records.php?id='.$row['id'].'"><button class="btn btn-primary">View</button></a></td>
                                         
                                         
                                     </tr>
