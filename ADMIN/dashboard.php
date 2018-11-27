1ffc086ae5da410c2c625f0191ebaa3e2e773f94
venessantonioo <venemanio98@gmail.com>
2018-11-27 15:47:45 +0800

    fixed dashboard

--- ADMIN/dashboard.php
+++ ADMIN/dashboard.php
@@ -110,88 +110,7 @@
       <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper">
-          <div class="row">
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-              <div class="card card-statistics">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-cube text-danger icon-lg"></i>
-                    </div>
-                    <div class="float-right">
-                      <p class="mb-0 text-right">Appointment Requests for today</p>
-                      <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0"><?php echo $box1['count']?></h3>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-alert-octagon mr-1" aria-hidden="true"></i> 65% lower growth
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-              <div class="card card-statistics">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-receipt text-warning icon-lg"></i>
-                    </div>
-                    <div class="float-right">
-                      <p class="mb-0 text-right">Vehicles to be repaired today</p>
-                      <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0"><?php echo $box2['count'];?></h3>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i> Product-wise sales
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-              <div class="card card-statistics">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-poll-box text-success icon-lg"></i>
-                    </div>
-                    <div class="float-right">
-                      <p class="mb-0 text-right">Total vehicles in progress</p>
-                      <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0"><?php echo $box3['count'];?></h3>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-calendar mr-1" aria-hidden="true"></i> Weekly Sales
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-              <div class="card card-statistics">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-account-location text-info icon-lg"></i>
-                    </div>
-                    <div class="float-right">
-                      <p class="mb-0 text-right">Total repaired vehicles</p>
-                      <div class="fluid-container">
-                        <h3 class="font-weight-medium text-right mb-0"><?php echo $box4['count']?></h3>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="text-muted mt-3 mb-0">
-                    <i class="mdi mdi-reload mr-1" aria-hidden="true"></i> Product-wise sales
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+         
 
         <!--  Start Calendar  -->
           <div class="row">
@@ -219,28 +138,87 @@
               <!--  Start  -->
               <div class="col-lg-4 grid-margin stretch-card">
               <div class="card">
-                
-                  <div class="card-body" >
-                  <h5 class="card-title text-primary mb-5">Feedback</h5>
-                  
-                <?php $feedback = $connection->prepare("SELECT * FROM feedback");
-                    if($feedback->execute()){
-                      $result=$feedback->get_result();
-                      while($feed = $result->fetch_assoc()){?>
-                          <div class="wrapper d-flex justify-content-between">
-                            <div class="side-left">
-                              <p class="mb-2" style="font-weight:bold"><?php echo $feed['name'];?></p>
-                              <p class="display-7 mb-4 font-weight-light"><?php echo $feed['message'];?></p>
-                            </div>
-                            <div class="side-right">
-                              <small class="text-muted"><?php echo $feed['email']?></small>
-                               <br> <small class="text-muted"><?php echo $feed['phoneNumber']?></small>
-                            </div>
-                          </div>
-                      <hr>
-                    <?php }
-                    }?>
+                     
+              <div class="card card-statistics">
+                <div class="card-body">
+                  <div class="clearfix">
+                    <div class="float-left">
+                      <i class="mdi mdi-calendar-clock text-danger icon-lg"></i>
+                    </div>
+                    <div class="float-right">
+                      <p class="mb-0 text-right">Appointment Requests for today</p>
+                      <div class="fluid-container">
+                        <h3 class="font-weight-medium text-right mb-0"><?php echo $box1['count']?></h3>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="text-muted mt-3 mb-0">
+                    <i class="mdi mdi-alert-octagon mr-1" aria-hidden="true"></i> 65% lower growth
+                  </p>
                 </div>
+              </div>
+                <hr>
+           
+              <div class="card card-statistics">
+                <div class="card-body">
+                  <div class="clearfix">
+                    <div class="float-left">
+                      <i class="mdi mdi-car-side text-warning icon-lg"></i>
+                    </div>
+                    <div class="float-right">
+                      <p class="mb-0 text-right">Vehicles to be repaired today</p>
+                      <div class="fluid-container">
+                        <h3 class="font-weight-medium text-right mb-0"><?php echo $box2['count'];?></h3>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="text-muted mt-3 mb-0">
+                    <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i> Product-wise sales
+                  </p>
+                </div>
+              </div>
+            <hr>
+            
+              <div class="card card-statistics">
+                <div class="card-body">
+                  <div class="clearfix">
+                    <div class="float-left">
+                      <i class="mdi mdi-wrench text-success icon-lg"></i>
+                    </div>
+                    <div class="float-right">
+                      <p class="mb-0 text-right">Total vehicles in progress</p>
+                      <div class="fluid-container">
+                        <h3 class="font-weight-medium text-right mb-0"><?php echo $box3['count'];?></h3>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="text-muted mt-3 mb-0">
+                    <i class="mdi mdi-calendar mr-1" aria-hidden="true"></i> Weekly Sales
+                  </p>
+                </div>
+              </div>
+           <hr>
+            
+              <div class="card card-statistics">
+                <div class="card-body">
+                  <div class="clearfix">
+                    <div class="float-left">
+                      <i class="mdi mdi-check-circle text-info icon-lg"></i>
+                    </div>
+                    <div class="float-right">
+                      <p class="mb-0 text-right">Total repaired vehicles</p>
+                      <div class="fluid-container">
+                        <h3 class="font-weight-medium text-right mb-0"><?php echo $box4['count']?></h3>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="text-muted mt-3 mb-0">
+                    <i class="mdi mdi-reload mr-1" aria-hidden="true"></i> Product-wise sales
+                  </p>
+                </div>
+              </div>
+          
+                  
               </div>
             </div>     
           </div>
@@ -251,7 +229,7 @@
             <div class="col-lg-12 grid-margin">
               <div class="card">
                 <div class="card-body">
-                  <h4 class="card-title">Orders</h4>
+                  <h4 class="card-title">Appointments</h4>
                   <div class="table-responsive">
                     <table class="table table-bordered">
                       <thead>
@@ -422,29 +400,34 @@
               </div>
             </div>
           </div>
-          <div class="row">
+          
+            <div class="row">
             <div class="col-12 grid-margin">
               <div class="card">
                 <div class="card-body">
-                  <h5 class="card-title mb-4">Manage Tickets</h5>
-                  <div class="fluid-container">
+                  <h5 class="card-title mb-4">Feedbacks</h5>
+                  <?php $feedback = $connection->prepare("SELECT * FROM feedback");
+                            if ($feedback->execute()){
+                                $result=$feedback->get_result();
+                                while($feed =$result->fetch_assoc()){?>
+                            
+                    <div class="fluid-container">
                     <div class="row ticket-card mt-3 pb-2 border-bottom pb-3 mb-3">
                       <div class="col-md-1">
-                        <img class="img-sm rounded-circle mb-4 mb-md-0" src="images/faces/face1.jpg" alt="profile image">
+                        <img class="img-sm rounded-circle mb-4 mb-md-0" src="images/faces/1.png" alt="profile image">
                       </div>
                       <div class="ticket-details col-md-9">
                         <div class="d-flex">
-                          <p class="text-dark font-weight-semibold mr-2 mb-0 no-wrap">James :</p>
-                          <p class="text-primary mr-1 mb-0">[#23047]</p>
-                          <p class="mb-0 ellipsis">Donec rutrum congue leo eget malesuada.</p>
+                          <p class="text-dark font-weight-semibold mr-2 mb-0 no-wrap"><?php echo $feed['name']?></p>
+                          <p class="text-primary mr-1 mb-0"></p>
+            
                         </div>
-                        <p class="text-gray ellipsis mb-2">Donec rutrum congue leo eget malesuada. Quisque velit nisi, pretium ut lacinia in, elementum id enim
-                          vivamus.
+                        <p class="text-gray ellipsis mb-2"><?php echo $feed['message']?>
                         </p>
                         <div class="row text-gray d-md-flex d-none">
                           <div class="col-4 d-flex">
-                            <small class="mb-0 mr-2 text-muted text-muted">Last responded :</small>
-                            <small class="Last-responded mr-2 mb-0 text-muted text-muted">3 hours ago</small>
+                            <small class="mb-0 mr-2 text-muted text-muted"><?php echo $feed['email']?></small>
+                            <small class="Last-responded mr-2 mb-0 text-muted text-muted"></small>
                           </div>
                           <div class="col-4 d-flex">
                             <small class="mb-0 mr-2 text-muted text-muted">Due in :</small>
@@ -452,110 +435,11 @@
                           </div>
                         </div>
                       </div>
-                      <div class="ticket-actions col-md-2">
-                        <div class="btn-group dropdown">
-                          <button type="button" class="btn btn-success dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Manage
-                          </button>
-                          <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-reply fa-fw"></i>Quick reply</a>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-history fa-fw"></i>Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-check text-success fa-fw"></i>Resolve Issue</a>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-times text-danger fa-fw"></i>Close Issue</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row ticket-card mt-3 pb-2 border-bottom pb-3 mb-3">
-                      <div class="col-md-1">
-                        <img class="img-sm rounded-circle mb-4 mb-md-0" src="images/faces/face2.jpg" alt="profile image">
-                      </div>
-                      <div class="ticket-details col-md-9">
-                        <div class="d-flex">
-                          <p class="text-dark font-weight-semibold mr-2 mb-0 no-wrap">Stella :</p>
-                          <p class="text-primary mr-1 mb-0">[#23135]</p>
-                          <p class="mb-0 ellipsis">Curabitur aliquet quam id dui posuere blandit.</p>
-                        </div>
-                        <p class="text-gray ellipsis mb-2">Pellentesque in ipsum id orci porta dapibus. Sed porttitor lectus nibh. Curabitur non nulla sit amet
-                          nisl.
-                        </p>
-                        <div class="row text-gray d-md-flex d-none">
-                          <div class="col-4 d-flex">
-                            <small class="mb-0 mr-2 text-muted">Last responded :</small>
-                            <small class="Last-responded mr-2 mb-0 text-muted">3 hours ago</small>
-                          </div>
-                          <div class="col-4 d-flex">
-                            <small class="mb-0 mr-2 text-muted">Due in :</small>
-                            <small class="Last-responded mr-2 mb-0 text-muted">2 Days</small>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="ticket-actions col-md-2">
-                        <div class="btn-group dropdown">
-                          <button type="button" class="btn btn-success dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Manage
-                          </button>
-                          <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-reply fa-fw"></i>Quick reply</a>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-history fa-fw"></i>Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-check text-success fa-fw"></i>Resolve Issue</a>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-times text-danger fa-fw"></i>Close Issue</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row ticket-card mt-3">
-                      <div class="col-md-1">
-                        <img class="img-sm rounded-circle mb-4 mb-md-0" src="images/faces/face3.jpg" alt="profile image">
-                      </div>
-                      <div class="ticket-details col-md-9">
-                        <div class="d-flex">
-                          <p class="text-dark font-weight-semibold mr-2 mb-0 no-wrap">John Doe :</p>
-                          <p class="text-primary mr-1 mb-0">[#23246]</p>
-                          <p class="mb-0 ellipsis">Mauris blandit aliquet elit, eget tincidunt nibh pulvinar.</p>
-                        </div>
-                        <p class="text-gray ellipsis mb-2">Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus. Lorem ipsum dolor sit amet.</p>
-                        <div class="row text-gray d-md-flex d-none">
-                          <div class="col-4 d-flex">
-                            <small class="mb-0 mr-2 text-muted">Last responded :</small>
-                            <small class="Last-responded mr-2 mb-0 text-muted">3 hours ago</small>
-                          </div>
-                          <div class="col-4 d-flex">
-                            <small class="mb-0 mr-2 text-muted">Due in :</small>
-                            <small class="Last-responded mr-2 mb-0 text-muted">2 Days</small>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="ticket-actions col-md-2">
-                        <div class="btn-group dropdown">
-                          <button type="button" class="btn btn-success dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Manage
-                          </button>
-                          <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-reply fa-fw"></i>Quick reply</a>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-history fa-fw"></i>Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-check text-success fa-fw"></i>Resolve Issue</a>
-                            <a class="dropdown-item" href="#">
-                              <i class="fa fa-times text-danger fa-fw"></i>Close Issue</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                    </div> 
                   </div>
+
+                    <?php }
+                            } ?>
                 </div>
               </div>
             </div>
